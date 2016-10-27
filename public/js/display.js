95a2f0b9ec8ec42c1ba196873704f5c18890d4ad
Femi Oladeji <femidotexe@gmail.com>
2016-10-27 13:22:54 +0100

    feature(display): Fix background image

--- public/js/display.js
+++ public/js/display.js
@@ -1,5 +1,5 @@
-// 'use strict';
-// $(document).ready(function() {
-//   $('.parallax').parallax();
-//   //$('.button-collapse').sideNav();
-// });
+'use strict';
+$(document).ready(function() {
+  $('.parallax').parallax();
+  //$('.button-collapse').sideNav();
+});
