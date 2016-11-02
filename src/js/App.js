e89cd8ee78b9f587c56a5e3f4affc36471245d7f
Joshua Azemoh <joshazemoh@gmail.com>
2016-11-02 15:48:28 +0100

    feature(signup): build signup component and setup route

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Router, Route, Link, hashHistory } from 'react-router';
 
-import Game from './Game';
+import Home from './components/Home';
+import Signup from './components/Signup';
 
-ReactDom.render(<Game />, document.getElementById('react-game'))
+ReactDom.render((
+  <Router history={hashHistory}>
+    <Route path='/' component={Home} />
+    <Route path='/signup' component={Signup} />
+    <Route path="*" component={Home} />
+  </Router>
+), document.getElementById('app'));
--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default class Game extends React.Component {
+export default class Index extends React.Component {
   render() {
     return (
       <div>
-        <h1>React Game</h1>
+        <h1>Cards for Humanity</h1>
       </div>
     )
   }
