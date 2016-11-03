c5c2baa5b6cb042ac7cc062b5fa6737e2031753d
Joshua Azemoh <joshazemoh@gmail.com>
2016-11-03 09:58:23 +0100

    chore(signup): add signup link to homepage

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class Index extends React.Component {
   render() {
     return (
       <div>
         <h1>Cards for Humanity</h1>
+        <Link to='/signup'>Sign up</Link>
       </div>
     )
   }
