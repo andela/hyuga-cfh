662b545fb6bfac8c370ea61bdddc97739ff9e3ee
Femi Oladeji <femidotexe@gmail.com>
2016-11-08 14:40:12 +0100

    Display(navigation): Add navigation menu and logo to Game

--- src/js/components/Game.js
+++ src/js/components/Game.js
@@ -2,8 +2,23 @@ import React, {Component} from "react";
 
 const Game = () => {
   return(
-    <div>
-      
+    <div className="main_background">
+      <nav>
+        <div className="nav-wrapper container">
+          <a href="#" className="brand-logo">
+            <img src="img/logo.png" />
+          </a>
+          <a id="mobile-menu-icon" data-activates="mobile-menu" className="button-collapse">
+            <i className="material-icons">menu</i>
+          </a>
+          <ul className="right hide-on-med-and-down">
+            <li><a href="exit">EXIT</a></li>
+          </ul>
+          <ul className="side-nav" id="mobile-menu">
+            <li><a href="exit">EXIT</a></li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 };
