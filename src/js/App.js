27dea3c248d48d8791204695240c0868575865dd
Femi Oladeji <femidotexe@gmail.com>
2016-11-08 14:22:27 +0100

    Display(react-setup): Setup Game components with react

--- src/js/App.js
+++ src/js/App.js
@@ -4,11 +4,13 @@ import { Router, Route, Link, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import Signup from './components/Signup';
+import Game from './components/Game';
 
 render((
   <Router history={hashHistory}>
     <Route path='/' component={Home} />
     <Route path='/signup' component={Signup} />
+    <Route path='/app' component={Game} />
     <Route path="*" component={Home} />
   </Router>
 ), document.getElementById('app'));
---
+++ src/js/components/Game.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+const Game = () => {
+  return(
+    <div>
+      
+    </div>
+  );
+};
+
+export default Game;
