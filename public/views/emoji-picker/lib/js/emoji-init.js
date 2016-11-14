4548625c1cde8ebc896c253da2229fc177f85121
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2016-11-14 20:31:36 +0100

    restructure emoji-picker files

--- public/views/emoji-picker/lib/js/emoji-init.js
+++ public/views/emoji-picker/lib/js/emoji-init.js
@@ -2,7 +2,7 @@ $(function () {
   // Initializes and creates emoji set from sprite sheet
   window.emojiPicker = new EmojiPicker({
     emojiable_selector: '[data-emojiable=true]',
-    assetsPath: 'views/lib/emoji-picker/img/',
+    assetsPath: 'views/emoji-picker/lib/img/',
     popupButtonClasses: 'fa fa-smile-o'
   });
   window.emojiPicker.discover();
